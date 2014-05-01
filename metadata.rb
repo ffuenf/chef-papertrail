e22018dcec2b2a23819d298f8002025d98fd983d
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-05-01 18:12:18 +0200

    add name attribute to make berkshelf happy

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "a.rosenhagen@ffuenf.de"
 license          "Apache 2.0"
 description      "installs/configures papertrail"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "papertrail"
 version          "1.0.0"
 
 %w{ debian ubuntu centos suse fedora redhat scientific amazon freebsd windows mac_os_x }.each do |os|
